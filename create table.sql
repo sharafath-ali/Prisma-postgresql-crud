e61a42db05d2174afb821f69ae2a0694c0f894f9
sharafath ali <sharafathaliofficial@gmail.com>
2025-03-10 01:01:52 +0530

    create table and insert and select query run on pgadmin

---
+++ create table.sql
@@ -0,0 +1,9 @@
+create table products(
+ p_name VARCHAR(200),
+ price integer
+);
+
+insert into products (p_name,price)
+VALUES ('jean', 40);
+
+INSERT INTO products (p_name, price) VALUES ('jean', 40);
