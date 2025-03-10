05b64bb0c083634298ed30c7150d4ee674e3ec56
sharafath ali <sharafathaliofficial@gmail.com>
2025-03-11 00:39:01 +0530

    alter columns in a table

--- add  new column using alter.sql
+++ add  new column using alter.sql
@@ -5,3 +5,5 @@ insert into products (p_name,price) values ('jean',80),('shirt',60);
 alter table products add column in_stock varchar(9);
 
 update products set in_stock = 90 where p_name = 'shirt' ;
+
+alter table products alter column in_stock type integer USING in_stock::integer;
